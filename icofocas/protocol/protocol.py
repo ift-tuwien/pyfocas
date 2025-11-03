b2137a8fe3ba588c6c71bb85494ab7712f56c5ed
Moritz Breurather <>
2025-11-03 12:10:24 +0100

    Cleanup on protocol.py

--- icofocas/protocol/protocol.py
+++ icofocas/protocol/protocol.py
@@ -2,14 +2,13 @@
 import logging
 import socket
 import struct
-from typing import Any, Optional
+from typing import Optional
 
 from icofocas.protocol.functions import FOCASFunction
-from icofocas.protocol.packet import ControlDevice, FOCASError, FOCASPacket, FOCASStatInfo, FOCASSysInfo, PacketType, \
-    PacketOrigin, \
-    RESPONSE_BUFFER_SIZE, \
-    extract_focas_packet, \
-    decode_scaled_integer, create_packet
+from icofocas.protocol.packet import ControlDevice, FOCASError, FOCASPacket, \
+    FOCASStatInfo, FOCASSysInfo, PacketType, PacketOrigin, \
+    RESPONSE_BUFFER_SIZE, extract_focas_packet, decode_scaled_integer, \
+    create_packet
 
 logger = logging.getLogger(__name__)
 
